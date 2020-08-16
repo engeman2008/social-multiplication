a0c14c26800c2c23f1b0ca155ba9510a92709fe8
Eman <eman.cse2008@gmail.com>
2020-08-16 15:27:56 +0200

    initial commit

---
+++ src/main/java/microservices/book/socialmultiplication/controller/MultiplicationResultAttemptController.java
@@ -0,0 +1,48 @@
+package microservices.book.socialmultiplication.controller;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import microservices.book.socialmultiplication.domain.MultiplicationResultAttempt;
+import microservices.book.socialmultiplication.service.MultiplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/results")
+public final class MultiplicationResultAttemptController {
+    private final MultiplicationService multiplicationService;
+
+    @Autowired
+    MultiplicationResultAttemptController(final MultiplicationService multiplicationService) {
+        this.multiplicationService = multiplicationService;
+    }
+
+    @PostMapping
+    ResponseEntity<MultiplicationResultAttempt> postResult(@RequestBody MultiplicationResultAttempt multiplicationResultAttempt) {
+        boolean isCorrect = multiplicationService.checkAttempt(multiplicationResultAttempt);
+        MultiplicationResultAttempt attemptCopy = new MultiplicationResultAttempt(
+                multiplicationResultAttempt.getUser(),
+                multiplicationResultAttempt.getMultiplication(),
+                multiplicationResultAttempt.getResultAttempt(),
+                isCorrect
+        );
+        return ResponseEntity.ok(attemptCopy);
+    }
+
+    @GetMapping
+    ResponseEntity<List<MultiplicationResultAttempt>> getStatistics(@RequestParam("alias") String alias) {
+        return ResponseEntity.ok(
+                multiplicationService.getStatsForUser(alias)
+        );
+    }
+    @GetMapping("/{resultId}")
+    ResponseEntity<MultiplicationResultAttempt> getResultById(final @PathVariable("resultId") Long resultId) {
+        return ResponseEntity.ok(
+                multiplicationService.getResultById(resultId)
+        );
+    }
+}
