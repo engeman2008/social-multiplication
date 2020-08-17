9d4ba681a1610655cdfd6b071af469e8a35d1107
Eman <eman.cse2008@gmail.com>
2020-08-17 08:16:01 +0200

    fix eureka and jackson json

--- src/main/java/microservices/book/socialmultiplication/configuration/WebConfiguration.java
+++ src/main/java/microservices/book/socialmultiplication/configuration/WebConfiguration.java
@@ -3,11 +3,11 @@ package microservices.book.socialmultiplication.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfiguration extends WebMvcConfigurerAdapter {
+public class WebConfiguration implements WebMvcConfigurer {
     /**
      * Enables Cross-Origin Resource Sharing (CORS)
      * More info: http://docs.spring.io/spring/docs/current/spring-framework-reference/html/cors.html
--- src/main/java/microservices/book/socialmultiplication/controller/MultiplicationController.java
+++ src/main/java/microservices/book/socialmultiplication/controller/MultiplicationController.java
@@ -1,8 +1,10 @@
 package microservices.book.socialmultiplication.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import microservices.book.socialmultiplication.domain.Multiplication;
 import microservices.book.socialmultiplication.service.MultiplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,18 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * This class implements a REST API for our Multiplication application.
  */
+@Slf4j
 @RestController
 @RequestMapping("/multiplications")
 public class MultiplicationController {
     private final MultiplicationService multiplicationService;
 
+    private final int serverPort;
+
     @Autowired
-    public MultiplicationController(final MultiplicationService multiplicationService){
+    public MultiplicationController(final MultiplicationService multiplicationService, @Value("${server.port}") int serverPort) {
         this.multiplicationService = multiplicationService;
+        this.serverPort = serverPort;
     }
 
     @GetMapping("/random")
-    Multiplication getRandomMultiplication(){
+    Multiplication getRandomMultiplication() {
+        log.info("Generating a random multiplication from server @ {}", serverPort);
         return multiplicationService.createRandomMultiplication();
     }
 }
