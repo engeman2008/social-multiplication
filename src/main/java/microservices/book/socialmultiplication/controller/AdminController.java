eb10a2d6dd3afac9f54e6c637489bfdf0fd4de7e
Eman <eman.cse2008@gmail.com>
2020-08-19 17:38:52 +0200

    profile test and admin rules

---
+++ src/main/java/microservices/book/socialmultiplication/controller/AdminController.java
@@ -0,0 +1,25 @@
+package microservices.book.socialmultiplication.controller;
+
+import microservices.book.socialmultiplication.service.AdminService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Profile("test")
+@RestController
+@RequestMapping("/multiplication/admin")
+public class AdminController {
+    private final AdminService adminService;
+
+    public AdminController(final AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/delete-db")
+    public ResponseEntity deleteDatabase() {
+        adminService.deleteDatabaseContents();
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/microservices/book/socialmultiplication/service/AdminService.java
@@ -0,0 +1,8 @@
+package microservices.book.socialmultiplication.service;
+
+public interface AdminService {
+    /**
+     * Deletes all the database contents
+     */
+    void deleteDatabaseContents();
+}
